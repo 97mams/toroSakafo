ac688bc2f94856579b9d027c36301f5201e646b6
97mams <97mams@gmail.com>
2024-03-19 09:49:24 +0300

    add feate CRUD

--- src/Controller/Admin/recipe/RecipeController.php
+++ src/Controller/Admin/recipe/RecipeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Admin\recipe;
 
+use App\Entity\Recipe;
+use App\Form\RecipeFormRequestType;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -10,10 +15,56 @@ use Symfony\Component\Routing\Attribute\Route;
 class RecipeController extends AbstractController
 {
     #[Route('/recipe', name: 'recipe.index')]
-    public function index(): Response
+    public function index(RecipeRepository $repository): Response
     {
+        $recipes = $repository->findAll();
+
         return $this->render('admin/recipe/recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
+            'recipes' => $recipes,
         ]);
+    }
+
+    #[Route('/recipe/ajout', name: 'recipe.create', requirements: ['GET', 'POST'])]
+    public function create(EntityManagerInterface $em, Request $request): Response
+    {
+        $recipe = new Recipe();
+        $formRecipe = $this->createForm(RecipeFormRequestType::class, $recipe);
+        $formRecipe->handleRequest($request);
+        if ($formRecipe->isSubmitted() && $formRecipe->isValid()) {
+            $em->persist($recipe);
+            $em->flush();
+
+            return $this->redirectToRoute('admin.recipe.index');
+        }
+
+        return $this->render('admin/recipe/recipe/index.html.twig', [
+            '$form' => $formRecipe,
+        ]);
+    }
+
+    #[Route('/recipe/edit/{id}', name: 'recipe.edit', requirements: ['GET', 'POST'])]
+    public function edit(Recipe $recipe, EntityManagerInterface $em, Request $request): Response
+    {
+        $formRecipe = $this->createForm(RecipeFormRequestType::class, $recipe);
+        $formRecipe->handleRequest($request);
+        if ($formRecipe->isSubmitted() && $formRecipe->isValid()) {
+            $em->persist($recipe);
+            $em->flush();
+
+            return $this->redirectToRoute('admin.recipe.index');
+        }
+
+        return $this->render('admin/recipe/recipe/index.html.twig', [
+            '$form' => $formRecipe,
+        ]);
+    }
+
+    #[Route('/recipe/suppression', name: 'recipe.delete', requirements: ['DELETE'])]
+    public function delete(Recipe $recipe, EntityManagerInterface $em)
+    {
+        $em->remove($recipe);
+        $em->flush();
+
+        return $this->redirectToRoute('admin.recipe.index');
     }
 }
